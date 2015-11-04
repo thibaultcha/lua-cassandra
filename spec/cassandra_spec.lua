fbf31630380f961c6d8453e32802f0f80eac79a8
Thibault Charbonnier <thibaultcha@me.com>
2015-11-04 01:33:37 -0800

    delete pre-rewrite files

--- spec/cassandra_spec.lua
+++
@@ -1,9 +0,0 @@
-local cassandra = require "cassandra"
-
-describe("Cassandra", function()
-  it("should have type annotation shortands", function()
-    assert.has_no_error(function()
-      cassandra.uuid()
-    end)
-  end)
-end)
--- src/cassandra/v2.lua
+++
@@ -1,3 +0,0 @@
-local _cassandra = require "_cassandra"
-
-return _cassandra("v2")

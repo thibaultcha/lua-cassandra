ddcdb1f4caf5562836d3869b070eb6fcd3eef005
Thibault Charbonnier <thibaultcha@me.com>
2016-01-15 19:14:11 -0800

    chore(ci) repeat tests three times

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ "$OPENRESTY_TESTS" != "yes" ]; then
   make lint
-  busted -v --coverage -o gtest
+  busted -v --coverage -o gtest --repeat 3
   luacov-coveralls -i cassandra
 else
   prove -l t

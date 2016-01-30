98d53dec8f33ade8167d1065cc51d2ed95935c64
Thibault Charbonnier <thibaultcha@me.com>
2016-01-29 16:02:51 -0800

    chore(ci) tests mainly run on 1 node cluster

--- .ci/setup_cassandra.sh
+++ .ci/setup_cassandra.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-arr=(${CASSANDRA_HOSTS//,/ })
+HOSTS=${CASSANDRA_HOSTS:-127.0.0.1}
+ARR=(${HOSTS//,/ })
 
 pip install --user PyYAML six
 git clone https://github.com/pcmanus/ccm.git
 pushd ccm
 ./setup.py install --user
 popd
-ccm create test -v binary:$CASSANDRA -n ${#arr[@]} -d
+ccm create test -v binary:$CASSANDRA -n ${#ARR[@]} -d
 ccm start -v
 ccm status

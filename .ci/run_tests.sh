5d28156d67fcb6cc2e65d7687de1dff40eec0f89
Thibault Charbonnier <thibaultcha@me.com>
2016-01-30 21:55:07 -0800

    chore(ci) ignore bit.lua coverage

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$OPENRESTY_TESTS" != "yes" ]; then
   make lint
   busted -v --coverage -o gtest --repeat 1
-  luacov-coveralls -i cassandra
+  luacov-coveralls -i src/cassandra -e bit.lua
 else
   ccm create resty_tests -v binary:$CASSANDRA -n 1
   ccm start --wait-for-binary-proto --wait-other-notice

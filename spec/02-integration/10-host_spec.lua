2912bf307760b6897444ed2139e875b1b132d476
Thibault Charbonnier <thibaultcha@me.com>
2016-03-07 21:44:44 -0800

    spec: use is_nil instead of falsy + ROWS parsing

--- spec/02-integration/10-host_spec.lua
+++ spec/02-integration/10-host_spec.lua
@@ -11,12 +11,11 @@ describe("host", function()
 
   describe("new()", function()
     it("create a peer", function()
-      local peer, err = host.new()
-      assert.falsy(err)
+      local peer = assert(host.new())
       assert.equal("127.0.0.1", peer.host)
       assert.equal(9042, peer.port)
       assert.equal(3, peer.protocol_version)
-      assert.falsy(peer.ssl)
+      assert.is_nil(peer.ssl)
       assert.truthy(peer.sock)
     end)
     it("accepts options", function()
@@ -25,11 +24,11 @@ describe("host", function()
         port = 9043,
         protocol_version = 2
       }
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.equal("192.168.1.1", peer.host)
       assert.equal(9043, peer.port)
       assert.equal(2, peer.protocol_version)
-      assert.falsy(peer.ssl)
+      assert.is_nil(peer.ssl)
       assert.truthy(peer.sock)
     end)
   end)
@@ -37,7 +36,7 @@ describe("host", function()
   describe("__tostring()", function()
     it("has a __tostring() metamethod", function()
       local peer, err = host.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local str = tostring(peer)
       assert.truthy(string.find(str, "<Cassandra socket: tcp{master}:"))
@@ -48,14 +47,14 @@ describe("host", function()
     it("errors if not initialized", function()
       local ok, err = host:connect()
       assert.equal("no socket created", err)
-      assert.falsy(ok)
+      assert.is_nil(ok)
     end)
     it("connects to a peer", function()
       local peer, err = host.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local ok, err = peer:connect()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.True(ok)
     end)
   end)
@@ -64,18 +63,18 @@ describe("host", function()
     it("errors if not initialized", function()
       local ok, err = host:close()
       assert.equal("no socket created", err)
-      assert.falsy(ok)
+      assert.is_nil(ok)
     end)
     it("closes a peer", function()
       local peer, err = host.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local ok, err = peer:connect()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.True(ok)
 
       ok, err = peer:close()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.equal(1, ok)
     end)
   end)
@@ -84,14 +83,14 @@ describe("host", function()
     it("errors if not initialized", function()
       local ok, err = host:settimeout()
       assert.equal("no socket created", err)
-      assert.falsy(ok)
+      assert.is_nil(ok)
     end)
     it("sets socket timeout", function()
       local peer, err = host.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local ok, err = peer:connect()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.True(ok)
 
       assert.has_no_error(function()
@@ -108,18 +107,18 @@ describe("host", function()
     it("errors if not initialized", function()
       local ok, err = host:setkeepalive()
       assert.equal("no socket created", err)
-      assert.falsy(ok)
+      assert.is_nil(ok)
     end)
     it("sets socket timeout", function()
       local peer, err = host.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local ok, err = peer:connect()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.True(ok)
 
       local ok, err = peer:setkeepalive()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.True(ok)
 
       finally(function()
@@ -132,10 +131,10 @@ describe("host", function()
     local peer
     setup(function()
       local p, err = host.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local _, err = p:connect()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       peer = p
     end)
@@ -147,14 +146,24 @@ describe("host", function()
       it("errors if not initialized", function()
         local ok, err = host:execute()
         assert.equal("no socket created", err)
-        assert.falsy(ok)
+        assert.is_nil(ok)
       end)
       it("executes a CQL query", function()
         local rows, err, code = peer:execute "SELECT * FROM system.local"
-        assert.falsy(err)
-        assert.falsy(code)
+        assert.is_nil(err)
+        assert.is_nil(code)
         assert.equal(1, #rows)
         assert.equal("ROWS", rows.type)
+
+        local row = rows[1]
+        assert.equal("local", row.key)
+      end)
+      it("parses ROWS results correctly", function()
+        local rows, err, code = peer:execute "SELECT * FROM system.local"
+        assert.is_nil(err)
+        assert.equal(1, #rows)
+        assert.equal("ROWS", rows.type)
+        assert.same({has_more_pages = false}, rows.meta)
 
         local row = rows[1]
         assert.equal("local", row.key)
@@ -165,13 +174,13 @@ describe("host", function()
           CREATE KEYSPACE IF NOT EXISTS ]]..tmp_name..[[
           WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1}
         ]])
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(0, #res)
         assert.equal("SCHEMA_CHANGE", res.type)
         assert.equal("CREATED", res.change_type)
         assert.equal("KEYSPACE", res.target)
         assert.equal(tmp_name, res.keyspace)
-        assert.falsy(res.name)
+        assert.is_nil(res.name)
 
         res, err = peer:execute([[
           CREATE TABLE ]]..tmp_name..[[.my_table(
@@ -179,7 +188,7 @@ describe("host", function()
             value int
           )
         ]])
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(0, #res)
         assert.equal("SCHEMA_CHANGE", res.type)
         assert.equal("CREATED", res.change_type)
@@ -188,45 +197,45 @@ describe("host", function()
         assert.equal("my_table", res.name)
 
         res, err = peer:execute("DROP KEYSPACE "..tmp_name)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(0, #res)
         assert.equal("SCHEMA_CHANGE", res.type)
         assert.equal("DROPPED", res.change_type)
         assert.equal("KEYSPACE", res.target)
         assert.equal(tmp_name, res.keyspace)
-        assert.falsy(res.name)
+        assert.is_nil(res.name)
       end)
       it("parses SET_KEYSPACE results", function()
         local peer_k, err = host.new()
-        assert.falsy(err)
+        assert.is_nil(err)
         local _, err = peer_k:connect()
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local tmp_name = os.tmpname():gsub("/", ""):lower()
         local res, err = peer_k:execute([[
           CREATE KEYSPACE IF NOT EXISTS ]]..tmp_name..[[
           WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1}
         ]])
-        assert.falsy(err)
+        assert.is_nil(err)
 
         res, err = peer_k:execute(string.format('USE "%s"', tmp_name))
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(0, #res)
         assert.equal("SET_KEYSPACE", res.type)
         assert.equal(tmp_name, res.keyspace)
 
         res, err = peer_k:execute("DROP KEYSPACE "..tmp_name)
-        assert.falsy(err)
+        assert.is_nil(err)
       end)
       it("returns CQL errors", function()
         local rows, err, code = peer:execute "SELECT"
-        assert.falsy(rows)
+        assert.is_nil(rows)
         assert.equal("[Syntax error] line 0:-1 no viable alternative at input '<EOF>'", err)
         assert.equal(host.cql_errors.SYNTAX_ERROR, code)
       end)
       it("binds args", function()
         local rows, err = peer:execute("SELECT * FROM system.local WHERE key = ?", {"local"})
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("local", rows[1].key)
       end)
     end) -- execute()
@@ -234,16 +243,16 @@ describe("host", function()
     describe("prepared queries", function()
       it("should prepare a query", function()
         local res, err = peer:prepare "SELECT * FROM system.local WHERE key = ?"
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.truthy(res.query_id)
         assert.equal("PREPARED", res.type)
       end)
       it("should execute a prepared query", function()
         local res, err = peer:prepare "SELECT * FROM system.local WHERE key = ?"
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local rows, err = peer:execute(res.query_id, {"local"}, {prepared = true})
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("local", rows[1].key)
       end)
     end)
@@ -251,18 +260,18 @@ describe("host", function()
     describe("set_keyspace()", function()
       it("sets a peer's keyspace", function()
         local peer_k, err = host.new()
-        assert.falsy(err)
+        assert.is_nil(err)
         local _, err = peer_k:connect()
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local res, err = peer_k:set_keyspace "system"
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(0, #res)
         assert.equal("SET_KEYSPACE", res.type)
         assert.equal("system", res.keyspace)
 
         local rows, err = peer_k:execute "SELECT * FROM local"
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("local", rows[1].key)
       end)
     end)
@@ -274,7 +283,7 @@ describe("host", function()
         utils.create_keyspace(peer, keyspace)
 
         local _, err = peer:set_keyspace(keyspace)
-        assert.falsy(err)
+        assert.is_nil(err)
 
         _, err = peer:execute [[
           CREATE TABLE IF NOT EXISTS things(
@@ -282,7 +291,7 @@ describe("host", function()
             n int
           )
         ]]
-        assert.falsy(err)
+        assert.is_nil(err)
 
         _, err = peer:execute [[
           CREATE TABLE IF NOT EXISTS counters(
@@ -290,7 +299,7 @@ describe("host", function()
             value counter
           )
         ]]
-        assert.falsy(err)
+        assert.is_nil(err)
       end)
 
       teardown(function()
@@ -307,11 +316,11 @@ describe("host", function()
           {"UPDATE things SET n = 2 WHERE id = "..uuid},
           {"UPDATE things SET n = 3 WHERE id = "..uuid}
         }
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("VOID", res.type)
 
         local rows, err = peer:execute("SELECT * FROM things WHERE id = "..uuid)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(3, rows[1].n)
       end)
       it("executes batch with params", function()
@@ -320,11 +329,11 @@ describe("host", function()
           {"INSERT INTO things(id, n) VALUES(?, ?)", {cassandra.uuid(uuid), 2}},
           {"INSERT INTO things(id, n) VALUES(?, ?)", {cassandra.uuid(uuid), 3}},
         })
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("VOID", res.type)
 
         local rows, err = peer:execute("SELECT * FROM things WHERE id = "..uuid)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(3, rows[1].n)
       end)
       it("executes an unlogged batch", function()
@@ -333,11 +342,11 @@ describe("host", function()
           {"UPDATE things SET n = 2 WHERE id = "..uuid},
           {"UPDATE things SET n = 3 WHERE id = "..uuid}
         }, {logged = false})
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("VOID", res.type)
 
         local rows, err = peer:execute("SELECT * FROM things WHERE id = "..uuid)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(3, rows[1].n)
       end)
       it("executes a counter batch", function()
@@ -346,11 +355,11 @@ describe("host", function()
           {"UPDATE counters SET value = value + 1 WHERE key = 'counter'"},
           {"UPDATE counters SET value = value + 1 WHERE key = 'counter'"}
         }, {counter = true})
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("VOID", res.type)
 
         local rows, err = peer:execute "SELECT value FROM counters WHERE key = 'counter'"
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(3, rows[1].value)
       end)
       it("supports protocol level timestamp", function()
@@ -360,10 +369,10 @@ describe("host", function()
           {"UPDATE things SET n = 2 WHERE id = "..uuid},
           {"UPDATE things SET n = 3 WHERE id = "..uuid}
         }, {timestamp = 1428311323417123})
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local rows, err = peer:execute("SELECT n,writetime(n) FROM things WHERE id = "..uuid)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(3, rows[1].n)
         assert.equal(1428311323417123, rows[1]["writetime(n)"])
       end)
@@ -373,18 +382,18 @@ describe("host", function()
           {"UPDATE things SET n = 2 WHERE id = "..uuid},
           {"UPDATE things SET n = 3 WHERE id = "..uuid}
         }, {serial_consistency = cassandra.consistencies.local_serial})
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local rows, err = peer:execute("SELECT * FROM things WHERE id = "..uuid)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(3, rows[1].n)
       end)
       it("execute prepared queries", function()
         local res1, err = peer:prepare "INSERT INTO things(id,n) VALUES(?,?)"
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local res2, err = peer:prepare "UPDATE things set n = ? WHERE id = ?"
-        assert.falsy(err)
+        assert.is_nil(err)
 
         local q1, q2 = res1.query_id, res2.query_id
 
@@ -395,20 +404,20 @@ describe("host", function()
           {q2, {4, cassandra.uuid(uuid)}},
           {q2, {5, cassandra.uuid(uuid)}}
         }, {prepared = true})
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal("VOID", res.type)
 
         local rows, err = peer:execute("SELECT * FROM things WHERE id = "..uuid)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.equal(5, rows[1].n)
       end)
       it("returns CQL errors", function()
         local res, err = peer:batch {
           {"INSERT FOO"}, {"INSERT BAR"}
         }
-        assert.falsy(res)
+        assert.is_nil(res)
         assert.equal("[Syntax error] line 0:-1 mismatched input '<EOF>' expecting '('", err)
       end)
-    end)
+    end) -- batch()
   end) -- CQL
 end)
--- spec/02-integration/11-cluster_spec.lua
+++ spec/02-integration/11-cluster_spec.lua
@@ -2,25 +2,24 @@ local utils = require "spec.spec_utils"
 local Cluster = require "cassandra.cluster"
 
 describe("cluster", function()
-  local hosts
   setup(function()
-    hosts = utils.ccm_start("cluster", 3)
+    utils.ccm_start("cluster", 3)
   end)
 
   describe("new()", function()
     it("creates a cluster with default options", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.truthy(cluster)
       assert.same({"127.0.0.1"}, cluster.contact_points)
-      assert.falsy(cluster.keyspace)
+      assert.is_nil(cluster.keyspace)
     end)
     it("accepts options", function()
       local cluster, err = Cluster.new {
         contact_points = {"127.0.0.2", "127.0.0.3"},
         keyspace = "system"
       }
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.same({"127.0.0.2", "127.0.0.3"}, cluster.contact_points)
       assert.equal("system", cluster.keyspace)
     end)
@@ -29,14 +28,14 @@ describe("cluster", function()
   describe("get_first_coordinator()", function()
     it("retrieves the first coordinator to respond", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local peer, err = cluster:get_first_coordinator {"127.0.0.255", "127.0.0.1"}
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.truthy(peer)
 
       local rows, err = peer:execute "SELECT * FROM system.peers"
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.equal(2, #rows)
 
       finally(function()
@@ -45,10 +44,10 @@ describe("cluster", function()
     end)
     it("returns nil when no coordinator replied", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local peer, err = cluster:get_first_coordinator {"127.0.0.254", "127.0.0.255"}
-      assert.falsy(peer)
+      assert.is_nil(peer)
       assert.equal("all hosts tried for query failed. 127.0.0.254: timeout 127.0.0.255: timeout", err)
     end)
   end)
@@ -56,20 +55,19 @@ describe("cluster", function()
   describe("refresh()", function()
     it("refreshes cluster infos in shm", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local ok, err = cluster:refresh()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.True(ok)
 
-      local shm = cluster.shm
       local cluster_infos, err = cluster:peers()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.same({"127.0.0.3", "127.0.0.2", "127.0.0.1"}, cluster_infos)
 
       for _, host in ipairs(cluster_infos) do
         local peer_infos, err = cluster:get_peer(host)
-        assert.falsy(err)
+        assert.is_nil(err)
         assert.same({reconnection_delay = 0, unhealthy_at = 0}, peer_infos)
       end
     end)
@@ -77,10 +75,10 @@ describe("cluster", function()
       local cluster, err = Cluster.new {
         contact_points = {"127.0.0.254", "127.0.0.255"}
       }
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local ok, err = cluster:refresh()
-      assert.falsy(ok)
+      assert.is_nil(ok)
       assert.equal("all hosts tried for query failed. 127.0.0.254: timeout 127.0.0.255: timeout", err)
     end)
   end)
@@ -88,34 +86,34 @@ describe("cluster", function()
   describe("get_next_coordinator()", function()
     it("complains if no hosts are in shm", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local peer, err = cluster:get_next_coordinator()
-      assert.falsy(peer)
+      assert.is_nil(peer)
       assert.equal("no hosts to try, must refresh", err)
     end)
     it("retrieves the next healthy peer from the load balancing policy", function()
       -- default is shm round robin policy
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local _, err = cluster:refresh()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local peer_1, err = cluster:get_next_coordinator()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.truthy(peer_1)
 
       local rows, err = peer_1:execute "SELECT * FROM system.peers"
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.equal(2, #rows)
 
       local peer_2, err = cluster:get_next_coordinator()
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.truthy(peer_2)
 
       rows, err = peer_2:execute "SELECT * FROM system.peers"
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.equal(2, #rows)
 
       assert.not_equal(peer_1.host, peer_2.host)
@@ -130,21 +128,21 @@ describe("cluster", function()
   describe("execute()", function()
     it("refreshes automatically if needed", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local rows, err = cluster:execute "SELECT * FROM system.peers"
-      assert.falsy(err)
+      assert.is_nil(err)
       assert.equal(2, #rows)
     end)
     it("selects the coordinator from the load balancing policy", function()
       -- default is shm round robin policy
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local s = spy.on(cluster, "get_next_coordinator")
 
       local _, err = cluster:execute "SELECT * FROM system.peers"
-      assert.falsy(err)
+      assert.is_nil(err)
 
       assert.spy(s).was.called()
     end)
@@ -153,10 +151,10 @@ describe("cluster", function()
   describe("shutdown()", function()
     it("flushes all the data in shms", function()
       local cluster, err = Cluster.new()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local _, err = cluster:refresh()
-      assert.falsy(err)
+      assert.is_nil(err)
 
       local keys = cluster.shm:get_keys()
       assert.not_same({}, keys)

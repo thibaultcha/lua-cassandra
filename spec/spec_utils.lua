8f014bfba3928b7bb80db85ae75abf156f770fee
Thibault Charbonnier <thibaultcha@me.com>
2016-08-19 15:39:50 -0700

    chore(ci) bump cassandra and openresty versions

--- spec/spec_utils.lua
+++ spec/spec_utils.lua
@@ -27,7 +27,7 @@ local LOAD = os.getenv("CASSANDRA_LOAD")
 local SSL_PATH = os.getenv("SSL_PATH") or "spec/fixtures/ssl"
 
 _M.n_inserts = LOAD and tonumber(LOAD) or 1000
-_M.CASSANDRA_VERSION = os.getenv("CASSANDRA") or "2.1.12"
+_M.CASSANDRA_VERSION = os.getenv("CASSANDRA") or "2.1.15"
 
 --- CCM
 
